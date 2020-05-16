da33b423ec06fd5cd59295fbd27adb60689a7769
JohanAndres19 <johanandresa@hotmail.com>
2020-05-16 16:26:23 -0500

    Desarrollo html y css

--- proyecto1.py
+++ proyecto1.py
@@ -1,15 +1,26 @@
-from proyecto2 import Grupo_musical    
+from proyecto2 import * 
+
 class Serenata ():   
-      def __init__(self):
-         self.Armar_Grupo()
+   def __init__(self):
+      self.Armar_Grupo()
 
-      def Armar_Grupo (self):
-           self.grupo = Grupo_musical()
+   def Armar_Grupo (self):
+       self.grupo = Grupo_musical()
       
-      def Dar_serenata(self):
-          pass
+   def Dar_serenata(self,opcion):
+     musicos =self.grupo.Get_musicos()
+    
+     if opcion is 1:
+            for i in musicos:
+              i.tocar()
+     else : 
+            for i in musicos:
+              i.PrepararInstrumento()  
+ 
 
-
-
-
+if __name__ == "__main__":
+    sere = Serenata()
+    print("ingrse 1 o 0")
+    num =int( input())
+    sere.Dar_serenata(num)
      
--- proyecto2.py
+++ proyecto2.py
@@ -1,19 +1,19 @@
-import numpy as np
 import random
-
 class Grupo_musical():
-     def __init__(self):
+    def __init__(self):
+         self.musicos_ins = []
          self.Orgarnizar_Grupo()
 
-     def Orgarnizar_Grupo(self):
-        num_musicos = random.randint(5,10)
-        musicos_ins = []
-        Instrumentos= np.array(["bateria","guitarra","piano","saxofon","violin"])
+    def Orgarnizar_Grupo(self):
+        num_musicos = random.randint(5,10) 
+        Instrumentos= [bateria(),guitarra(),piano(),saxofon(),violin()]
         for i in range (1,num_musicos):
             tipo_instru = random.randint(0,4)
             instru = Instrumentos[tipo_instru]  
-            musicos_ins.append(instru)  
-         
+            self.musicos_ins.append(instru) 
+
+    def Get_musicos(self):
+        return self.musicos_ins
             
 class Instrumento():
     def tocar(self):
@@ -23,24 +23,35 @@ class Instrumento():
     
 class bateria(Instrumento):
     def tocar(self):
-        print(" preparando bateria ")
+        print(" tocando bateria ")
 
     def PrepararInstrumento(self):
         print(" preparando bateria ")
 
 class piano(Instrumento):
+    def tocar(self):
+        print(" tocando piano ")
     
     def PrepararInstrumento(self):
         print(" preparando piano ")
 
 class saxofon(Instrumento):
-     def PrepararInstrumento(self):
+    def tocar(self):
+        print(" tocando saxofon ")
+
+    def PrepararInstrumento(self):
         print("preparando saxofon")
 
 class guitarra(Instrumento):
+    def tocar(self):
+        print(" tocando guitarra ")
+
     def PrepararInstrumento(self):
         print("preparando guitarra")
 
 class violin(Instrumento):
+    def tocar(self):
+        print(" tocando violin ")
+
     def PrepararInstrumento(self):
          print("preparando violin ")
